fb4f8f2facbb85d5e1e2ad6e1c33d677d90795fb
aliflazuardi <aliflazuardi250999@gmail.com>
2024-05-15 21:56:26 +0700

    implement structure in c

--- c/structure.c
+++ c/structure.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+struct Rectangle {
+  int length;
+  int breadth;
+  char x[4];
+};
+
+
 int main() {
+  struct Rectangle r1={10,5,"abcd"};
+
+  printf("%ld\n", sizeof(r1));
+
+  printf("%c\n", r1.x[0]);
+
+  printf("Area: %d\n", r1.length * r1.breadth);
 
   return 0;
 }
