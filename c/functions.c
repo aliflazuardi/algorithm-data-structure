f36480cb6d55f95d23f448450c500171cdacb918
aliflazuardi <aliflazuardi250999@gmail.com>
2024-05-20 08:07:37 +0700

    add function implementation in c

---
+++ c/functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+  int c;
+  c = a + b;
+  return c;
+}
+
+int main() {
+  int num1=10,num2=15,sum;
+
+  sum = add(num1, num2);
+
+  printf("%d\n", sum);
+
+  return 0;
+}
