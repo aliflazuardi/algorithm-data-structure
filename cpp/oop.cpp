c2a59738ec86abb6c7cb1ccdf7a5563b688f1845
aliflazuardi <aliflazuardi250999@gmail.com>
2024-05-25 17:28:02 +0700

    implment oop principles in cpp using struct

--- cpp/oop.cpp
+++ cpp/oop.cpp
@@ -6,23 +6,23 @@ using namespace std;
 struct Rectangle {
   int length;
   int breadth;
+
+  void initialise(int l, int b) {
+    length = l;
+    breadth = b;
+  }
+
+  int area() {
+    return length * breadth;
+  }
+
+  int perimeter() {
+    int p;
+
+    p = 2 * (length+breadth);
+    return p;
+  }
 };
-
-void initialise(struct Rectangle *r, int l, int b) {
-  r->length = l;
-  r->breadth = b;
-}
-
-int area(struct Rectangle r) {
-  return r.length * r.breadth;
-}
-
-int perimeter(struct Rectangle r) {
-  int p;
-
-  p = 2 * (r.length+r.breadth);
-  return p;
-}
 
 int main() {
   Rectangle r={0,0};
@@ -31,10 +31,10 @@ int main() {
   printf("Enter length and breadth\n");
   cin>>l>>b;
 
-  initialise(&r, l, b);
+  r.initialise(l, b);
 
-  int a = area(r);
-  int peri = perimeter(r);
+  int a = r.area();
+  int peri = r.perimeter();
 
   printf("Area: %d\nPerimeter: %d\n", a, peri);
 
